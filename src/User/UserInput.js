5c60e3d5ebc6729f1461c53beb0741dac4e4f4d0
garfield <cmgarfield@gmail.com>
2018-04-14 22:35:52 +0800

    Create TWO new components: UserInput and UserOutput

---
+++ src/User/UserInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const userInput = () => {
+  return(
+    <h1>userInput</h1>
+  );
+};
+
+export default userInput;
---
+++ src/User/UserOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const userOutput = () => {
+  return(
+    <h1>userOutput</h1>
+  );
+};
+
+export default userOutput;
